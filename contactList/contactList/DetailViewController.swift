249b868384f902780b1ffc1109d26f9ba505c2c8
Farzin Fakhraei <ffakhrea@gmail.com>
2018-10-14 01:36:43 -0700

    initial commit - lab 5

---
+++ contactList/contactList/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  contactList
+//
+//  Created by user145368 on 10/9/18.
+//  Copyright © 2018 Farzin Fakhraei. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ contactList/contactListTests/contactListTests.swift
@@ -0,0 +1,34 @@
+//
+//  contactListTests.swift
+//  contactListTests
+//
+//  Created by user145368 on 10/9/18.
+//  Copyright © 2018 Farzin Fakhraei. All rights reserved.
+//
+
+import XCTest
+@testable import contactList
+
+class contactListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
